7c945d7469422a481f8814690fc99e2a7861f074
Hugo Thiago <103154103+Huglh@users.noreply.github.com>
2024-03-12 22:36:57 -0300

    Add files via upload

---
+++ TiposAbstratosDeDados/NoLista.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiposAbstratosDeDados
+{
+    class NoLista
+    {
+        public int chave;
+        public string nome;
+        public NoLista prox;
+
+        public NoLista(int chave, string nome)
+        {
+            this.chave = chave;
+            this.nome = nome;
+            this.prox = null;
+        }
+    }
+
+    class Lista
+    {
+        private NoLista prim;
+        private NoLista ult;
+
+        public Lista()
+        {
+            prim = ult = null;
+        }
+
+        public void Inserir(NoLista item)
+        {
+            if (prim == null)
+                prim = item;
+            else
+                ult.prox = item;
+            ult = item;
+        }
+
+        public void Imprimir()
+        {
+            NoLista aux = prim;
+            while (aux != null)
+            {
+                Console.WriteLine(aux.chave + ", " + aux.nome);
+                aux = aux.prox;
+            }
+        }
+
+        public NoLista Pesquisar(int chave)
+        {
+            NoLista aux = prim;
+            while (aux != null && aux.chave != chave)
+            {
+                aux = aux.prox;
+            }
+            return aux;
+        }
+
+        public bool Remover(int chave)
+        {
+            NoLista aux = prim, ant = null;
+            while (aux != null && aux.chave != chave)
+            {
+                ant = aux;
+                aux = aux.prox;
+            }
+            if (aux != null)
+            {
+                if (ant != null) // não é o primeiro 
+                    ant.prox = aux.prox;
+                else // é o primeiro
+                    prim = aux.prox;
+                if (aux == ult) // é o último
+                    ult = ant;
+                aux.prox = null; // desconecta o nó da lista
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ TiposAbstratosDeDados/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace TiposAbstratosDeDados
+{
+    class Program  //Pratica3
+    {
+        static void Main(string[] args)
+        {
+            Lista l = new Lista();
+            int opcao = 0;
+
+            while (opcao != 4)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1) Inserir");
+                Console.WriteLine("2) Pesquisar");
+                Console.WriteLine("3) Imprimir Lista");
+                Console.WriteLine("4) Sair");
+                Console.Write("Digite a opção desejada: ");
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Digite um número: ");
+                        int chave = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Digite um nome: ");
+                        string nome = Console.ReadLine();
+                        if (l.Pesquisar(chave) != null)
+                        {
+                            Console.WriteLine("Erro: chave duplicada!");
+                        }
+                        else
+                        {
+                            l.Inserir(new NoLista(chave, nome));
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Digite um número a ser pesquisado: ");
+                        chave = Convert.ToInt32(Console.ReadLine());
+                        NoLista n = l.Pesquisar(chave);
+                        if (n == null)
+                            Console.WriteLine("Valor não encontrado!");
+                        else
+                        {
+                            Console.WriteLine("Achou: " + n.chave + ", " + n.nome);
+                            Console.Write("Deseja remover o nó encontrado? (S/N): ");
+                            string opcaoRemover = Console.ReadLine();
+                            if (opcaoRemover.ToUpper() == "S")
+                            {
+                                l.Remover(chave);
+                                Console.WriteLine("Nó removido.");
+                            }
+                        }
+                        break;
+                    case 3:
+                        l.Imprimir();
+                        break;
+                    case 4:
+                        Console.WriteLine("Encerrando o programa.");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
+
